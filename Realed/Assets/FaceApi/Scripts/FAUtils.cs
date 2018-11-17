4d490b06c66af07ff5a393bc407bbda058895bb7
warrior97 <dbobicic@gmail.com>
2018-11-17 14:08:23 +0100

    [+] Addes FACEAPI scripts

---
+++ Realed/Assets/FaceApi/Scripts/FAUtils.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FAUtils {
+
+	public static double getConfidenceForCompare(byte[] image)
+    {
+       // string fp0 = "C:\\Users\\bobic\\Desktop\\SLIKE\\img00.jpg";
+        ///string fp1 = "C:\\Users\\bobic\\Desktop\\SLIKE\\img01.jpg";
+       // byte[] imgD0 = File.ReadAllBytes(fp0);
+        //byte[] imgD1 = File.ReadAllBytes(fp1);
+        FARequest frf = new FARequest(APIConstants.APIKEY, APIConstants.APISECRET);
+        frf.image_file1 = image;
+        frf.image_file2 = ;
+
+        var obj = GameObject.Find("HttpUtil");
+        HTTPUtils htp = obj.GetComponent<HTTPUtils>();
+        htp.POST(APIConstants.APIURL_COMPARE, frf);
+        while (!htp) { }
+        double data = htp.confData;
+        return data;
+    }
+}
---
+++ Realed/Assets/FaceApi/Scripts/StartScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello");
+        
+        string fp0 = "C:\\Users\\bobic\\Desktop\\SLIKE\\img00.jpg";
+        string fp1 = "C:\\Users\\bobic\\Desktop\\SLIKE\\img01.jpg";
+        byte[] imgD0 = File.ReadAllBytes(fp0);
+        byte[] imgD1 = File.ReadAllBytes(fp1);
+        FARequest frf=new FARequest(APIConstants.APIKEY, APIConstants.APISECRET);
+        frf.image_file1 = imgD0;
+        frf.image_file2 = imgD1;
+
+        var obj = GameObject.Find("HttpUtil");
+        HTTPUtils htp=obj.GetComponent<HTTPUtils>();
+        htp.POST(APIConstants.APIURL_COMPARE, frf);
+        while (!htp) { }
+        double data
+       // frf.image_file1
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
